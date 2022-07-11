7f11d5c482f01f5d4755791db4764d0e66798c8d
gurinina <g.giaever@ubc.ca>
2022-07-11 13:14:00 -0700

    initial commit

---
+++ R/data.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+
+shrimp_raw <- readr::read_csv("data-raw/shrimp.csv")
+
+# Add any tidying steps to this script if necessary
+shrimp <- shrimp_raw[-c(1:2),]
+colnames(shrimp) <- c("Year", "Month", shrimp_raw[1,3:10])
+shrimp <- readr::type_convert(shrimp)
